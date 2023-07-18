8b47a9294665ac53aa085706ce2dcc9869ba5ac2
Chad Bowler <chadbowler@gmail.com>
2023-07-18 15:45:53 -0600

    added new database info and JAWSBD into config

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_AMBER) {
+  sequelize = new Sequelize(process.env.JAWSDB_AMBER);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
